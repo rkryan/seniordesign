9a15a0f1e423ce330e18dddb51d997d2678aa872
Ryan King <rk.ryan.king@gmail.com>
2013-11-21 18:09:26 +0000

    Added initial server code

---
+++ bluetooth/server/src/RemoteBluetoothServer.java
@@ -0,0 +1,9 @@
+package bluetooth;
+
+public class RemoteBluetoothServer {
+    
+    public static void main(String[] args) {
+	Thread waitThread = new Thread(new WaitThread());
+	waitThread.start();
+    }
+}
---
+++ bluetooth/server/src/WaitThread.java
@@ -0,0 +1,52 @@
+package bluetooth;
+
+import java.io.IOException;
+
+import javax.bluetooth.BluetoothStateException;
+import javax.bluetooth.DiscoveryAgent;
+import javax.bluetooth.LocalDevice;
+import javax.bluetooth.UUID;
+import javax.microedition.io.Connector;
+import javax.microedition.io.StreamConnection;
+import javax.microedition.io.StreamConnectionNotifier;
+
+public class WaitThread implements Runnable {
+    
+    public WaitThread() {
+    }
+
+    public void run() {
+	waitForConnection();
+    }
+
+    private void waitForConnection() {
+	LocalDevice local = null;
+	
+	StreamConnectionNotifier notifier;
+	StreamConnection connection = null;
+
+	try {
+	    local = LocalDevice.getLocalDevice();
+	    local.setDiscoverable(DiscoveryAgent.GIAC);
+	    UUID uuid = new UUID(80087355);
+	    String url = "btspp://localhost:" + uuid.toString() + ";name=RemoteBluetooth";
+	    notifier = (StreamConnectionNotifier)Connector.open(url);
+	} catch(Exception ex) {
+	    ex.printStackTrace();
+	    return;
+	}
+
+	while(true) {
+	    try {
+		System.out.println("waiting for connection...");
+		connection = notifier.acceptAndOpen();
+		Thread processThread = new Thread(new ProcessConnectionThread(connection));
+		processThread.start();
+	    } catch (Exception ex) {
+		ex.printStackTrace();
+		return;
+	    }
+	}
+    }
+}
+	
