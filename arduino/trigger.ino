4258f3d5e90820fc73b30880edd7caa97804fb47
Ryan King <rk.ryan.king@gmail.com>
2013-11-22 09:39:14 -0500

    Added arduino code to trigger line voltage

---
+++ arduino/trigger.ino
@@ -0,0 +1,27 @@
+int outpin = 4;
+volatile int flag = 1;
+
+void setup()
+{
+  // Serial.begin(9600);
+  attachInterrupt(0,zero_crossing,CHANGE);
+  pinMode(outpin,OUTPUT);
+}
+
+void loop()
+{
+  if(flag == 0)
+  {
+    delay(4);
+    digitalWrite(outpin,HIGH);
+    // Serial.println("Trigger");    
+    flag = 1;
+  }
+}
+
+
+void zero_crossing()
+{
+  digitalWrite(outpin,LOW);
+  flag = 0;
+}
