9e57f5f444e460789db2a1dbc6589f0aec431eee
Ryan King <rk.ryan.king@gmail.com>
2013-12-08 02:17:59 -0500

    Documented send_string

--- pi/serial/send_string.py
+++ pi/serial/send_string.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python
 
+# Simple script to send an input string out of the raspberry pi over UART.
+# A string is sent as a series of ASCII bytes and can be of arbitrary length.
+# The signal is sent over GPIO 14 (Tx) at 1200 baud.  The string send is a command line argument.
+# This script opens up the appropriate connection, sets the settings, 
+# writes the word, and closes the connection.
+
 import serial, sys
 
 output = sys.argv[1]
 
-ser = serial.Serial("/dev/ttyAMA0")
-ser.baudrate = 1200
+ser = serial.Serial("/dev/ttyAMA0") # opens serial link over UART Tx pin
+ser.baudrate = 1200 # predetermined baud rate
 ser.write(output)
+# print "Sending:"
+# print output
 ser.close()
