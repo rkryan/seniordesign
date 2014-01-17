3eef1385c3f23e44986477c9227eaa2c97514c93
Ryan King <rk.ryan.king@gmail.com>
2014-01-17 16:52:16 -0500

    Began working on two way communication

---
+++ pi/enable/receive.java
@@ -0,0 +1,24 @@
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.PinState;
+import com.pi4j.io.gpio.RaspiPin;
+
+public class receive {
+    
+    public static void main(String[] args) throws InterruptedException {
+
+	// create gpio controller
+	GpioController gpio = GpioFactory.getInstance();
+        
+	// receive control signal is in GPIO_05, or GPIO 25 on the board
+	// transmit control signal is in GPIO_06, or GPIO 24 on the board
+	GpioPinDigitalOutput receivePin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_05, "MyPin", PinState.LOW);
+	GpioPinDigitalOutput transmitPin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_06, "MyPin", PinState.LOW);
+
+	// enable receiving and disable transmitting
+	receivePin.high();
+	transmitPin.low();
+
+    }
+}
---
+++ pi/serial/read_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+# Simple script to send an input string out of the raspberry pi over UART.
+# A string is sent as a series of ASCII bytes and can be of arbitrary length.
+# The signal is sent over GPIO 14 (Tx) at 1200 baud.  The string send is a command line argument.
+# This script opens up the appropriate connection, sets the settings, 
+# writes the word, and closes the connection.
+
+import serial, sys
+
+output = sys.argv[1]
+
+ser = serial.Serial("/dev/ttyAMA0") # opens serial link over UART Tx pin
+ser.baudrate = 1200 # predetermined baud rate
+while 1:
+    print ser.readline()
